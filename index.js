366fc59d3efc0545654a069fb708143ab6c3da1d
InkoHX <inkohx@gmail.com>
2018-08-02 10:57:11 +0900

    簡単に作りました。

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-
+const DiscordJS = require(`discord.js`)
+const client = new DiscordJS.Client()
+const { parsed: Config } = require(`dotenv`).load()
+const Prefix = `v!`
+client.on(`ready`, () => {
+  console.log(`起動完了`)
+}).on(`message`, (msg) => {
+  if (!msg.guild) return
+  if (msg.author.bot) return
+  if (!msg.content.startsWith(Prefix)) return
+  const [command, ...args] = msg.content.slice(Prefix.length).split(` `)
+  switch (command) {
+  case `report`:
+    msg.delete()
+    let args = msg.content.split(` `)
+    if (args[1]) {
+      if (args[2]) {
+        msg.reply(`報告ありがとうございます。(\`･ω･´)b`)
+        client.channels.get(`439369239594270740`).send(`\n**${msg.author.tag}**からの報告\n違反者: **${args[1]}**\n理由: ${args[2]}`)
+      } else {
+        msg.reply(`使い方: v!report <プレイヤーの名前> <理由>`)
+      }
+    } else {
+      msg.reply(`使い方: v!report <プレイヤーの名前> <理由>`)
+    }
+    break
+  }
+})
+client.login(Config.TOKEN)
